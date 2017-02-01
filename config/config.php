3b379bc802a37e561d246dd824b7638912aa4d44
ankitm123 <amohapatra2012@my.fit.edu>
2017-02-01 09:24:23 -0500

    Switched from Symphony routing to Nikic/Fastroute

--- config/config.php
+++ config/config.php
@@ -1,16 +1,16 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: ankit
-     * Date: 1/9/17
-     * Time: 2:39 AM
-     */
-    /* Put your configuration here! */
-    return array(
-        'database'  => array(
-            'dbhost'    => 'localhost',
-            'dbname'    => 'Some name',
-            'dbuser'    => 'Some user',
-            'dbpass'    => 'Good password'
-        )
-    );
+	/**
+	 * Created by PhpStorm.
+	 * User: ankit
+	 * Date: 1/9/17
+	 * Time: 2:39 AM
+	 */
+	/* Put your configuration here! */
+	return [
+		'database' => [
+			'dbhost' => 'localhost',
+			'dbname' => 'Some name',
+			'dbuser' => 'Some user',
+			'dbpass' => 'Good password'
+		]
+	];
