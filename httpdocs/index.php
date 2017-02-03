d96878adc58896a92b430ee308277cf9b304cc08
ankitm123 <amohapatra2012@my.fit.edu>
2017-02-03 09:40:26 -0500

    Some basic twig

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,62 +1,46 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: ankit
-     * Date: 1/5/17
-     * Time: 5:10 PM
-     */
+	/**
+	 * Created by PhpStorm.
+	 * User: ankit
+	 * Date: 2/3/17
+	 * Time: 9:40 AM
+	 */
 
-    require_once '../vendor/autoload.php';
-    use Symfony\Component\HttpFoundation\Request;
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\RequestContext;
-    use Symfony\Component\Routing\Matcher\UrlMatcher;
+	require_once '../Troodon/bootstrap.php';
 
-    /**
-     * @param $str
-     * @param string $encoding
-     * @return string
-     */
-    function noHTML($str, $encoding = 'UTF8')
-    {
-        return htmlentities($str, ENT_QUOTES | ENT_HTML5, $encoding);
-    }
+	/* Create a troodon App */
+	//$app = new App;
+	/* Get http request method and URI from HTTPFoundation object  */
+	$httpMethod = $request->getMethod();
+	$uri = $request->getRequestUri();
 
-    /**
-     * @param $param
-     * @return Response
-     */
-    function render_template($param){
-        extract($param->attributes->all(),EXTR_SKIP);
-        ob_start();
-        include '../src/views/' . $_route . '.php';
-        return new Response(ob_get_clean());
-    }
-
-    /* Create a request */
-    $request    = Request::createFromGlobals();
-
-    /* Get the routesCollection route file */
-    $routes     = include '../src/route.php';
-
-    /* Create an instance of request context */
-    $context    = new RequestContext();
-
-    /* Create a context from the request */
-    $context->fromRequest($request);
-
-    /* Instantiate UrlMatcher */
-    $matcher    = new UrlMatcher($routes, $context);
-    try{
-        $request->attributes->add($matcher->match($request->getPathInfo()));
-        $response   = call_user_func($request->attributes->get('_controller'),$request);
-    }catch (Symfony\Component\Routing\Exception\ResourceNotFoundException $e){
-        $response   = new Response('Page not found', '404');
-    }catch(Exception $e){
-        $response   = new Response('An error occured', '500');
-    }
-    /* Set response */
-
-    $response->send();
+	// Strip query string (?name=ankit) and decode URI
+	if (false !== $pos = strpos($uri, '?')) {
+		$uri = substr($uri, 0, $pos);
+	}
+	$uri = rawurldecode($uri);
 
 
+
+	/* Dispatch the simple dispatcher object */
+	$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+	switch ($routeInfo[0]) {
+		case FastRoute\Dispatcher::NOT_FOUND:
+			// Load 404 page
+			break;
+		case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+			$allowedMethods = $routeInfo[1];
+			// Not sure what to do with this
+			break;
+		case FastRoute\Dispatcher::FOUND:
+			$handler = $routeInfo[1];
+			$vars = $routeInfo[2];
+			// ... call $handler with $vars
+			break;
+	}
+
+
+
+
+	//echo $view;
